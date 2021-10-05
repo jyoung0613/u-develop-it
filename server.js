c6f5153a7dd7b1661912f07785f96cf0f5f770eb
John Young <87405768+jyoung0613@users.noreply.github.com>
2021-10-05 12:33:13 -0600

    added parties table and then left joined with party id to combine the data i wanted with the other candidates table

--- server.js
+++ server.js
@@ -24,7 +24,12 @@ const db = mysql.createConnection(
 
 // Get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name
+    AS party_name
+    FROM candidates
+    LEFT JOIN parties
+    ON candidates.party_id = parties.id
+    WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
@@ -86,7 +91,11 @@ app.post('/api/candidate', ({ body }, res) => {
 
 // Get all canidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name
+    As party_name
+    FROM candidates
+    LEFT JOIN parties
+    ON candidates.party_id = parties.id`;
 
     db.query(sql, (err, rows) => {
         if (err) {
